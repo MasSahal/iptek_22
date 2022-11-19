4981ed929f585805580a0ef8b35b618ca3b5e8ab
Mas Sahal <abusahl444@gmail.com>
2022-11-19 13:45:59 +0700

    rev p3 process

--- kelas_dasar/proses.php
+++ kelas_dasar/proses.php
@@ -1,2 +1,31 @@
-<?php 
+<?php
 
+if (isset($_POST['login'])) {
+
+    // ambil data username
+    $username = $_POST['username'];
+
+    // ambil data password
+    $password = $_POST['password'];
+
+    if ($username == "admin") {
+
+        if ($password == "admin") {
+            echo "<h1>Selamat datang Admin!</h1>";
+        } else {
+            echo "<h1>Oops! password salah!</h1>";
+        }
+    } else {
+        echo "<h1>Oops! username salah!</h1>";
+    }
+
+    echo "<br>";
+    echo "<a href='index.php'>Kembali</a>";
+}
+
+
+    // if ($username == "admin" and $password == "admin") {
+    //     echo "<h1>Selamat datang Admin!</h1>";
+    // } else {
+    //     echo "<h1>Oops! username atau password salah!</h1>";
+    // }
